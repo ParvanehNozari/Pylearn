e9483cd5d70d7099e15195f137a7e54dfc3ed470
Parvaneh Nozari <nozariparvaneh@gmail.com>
2024-11-29 23:58:59 +0330

    pylearn

--- 1.7/1.7/7.4_Generate_a_chessboard_pattern.py
+++ 1.7/1.7/7.4_Generate_a_chessboard_pattern.py
@@ -1,11 +1,20 @@
 n = int(input("Enter the number of rows (n): "))
 m = int(input("Enter the number of columns (m): "))
 
-for i in range(n):  
-    row = ""  
-    for j in range(m): 
-        if (i + j) % 2 == 0:  
-            row += "⬜"  
+
+for i in range(n):
+    row = ""
+    for j in range(m):
+        if j != m:
+            if (i+j) % 2 == 0:
+                row += "⬜"
+            else:
+                row += "⬛" 
         else:
-            row += "⬛"  
-    print(row)  
+            if (i+j) % 2== 0:
+                row += "⬜\n"
+            else:
+                row += "⬛\n" 
+    print(row)
+
+
